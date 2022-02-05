08d9b3c46ccba74926dd83c95b8d1e8a82833214
riniaokyama <rinia.sns@gmail.com>
2022-02-05 20:42:48 +0900

    update

--- generator.swift
+++ generator.swift
@@ -9,7 +9,7 @@ struct RemoteJS: Codable {
 
 let remoteJss = [
     RemoteJS(id: "6251e263c1ee45ef94894399898c6b2c", title: "ミリ秒表示", version: 1, jsUrl: ""),
-    RemoteJS(id: "f3beff0c72184100bb4f1f55a1147586", title: "ナビゲーションタブ", version: 2, jsUrl: "https://raw.githubusercontent.com/RiniaOkyama/MarinDeck4iOS-RemoteJS/main/js/navigationTab_v03.js"),
+    RemoteJS(id: "f3beff0c72184100bb4f1f55a1147586", title: "ナビゲーションタブ", version: 5, jsUrl: "https://raw.githubusercontent.com/RiniaOkyama/MarinDeck4iOS-RemoteJS/main/js/navigationTab_v05.min.js"),
     RemoteJS(id: "1923ebc903de49269205b9b8e345c091", title: "ツイート画面を下に持ってくる", version: 1, jsUrl: ""),
     RemoteJS(id: "25554b54c7b848e3b027bb097f4a37d9", title: "緊急用", version: 1, jsUrl: ""),
 ]
